ba5b26bde102266eea48c4f76862d462ced03332
Kahkeshan Begum <62101356+Kahkeshan04@users.noreply.github.com>
2023-11-15 10:57:42 +0530

    Update SQLAdvance.sql

--- Tutorials/SQLAdvance.sql
+++ Tutorials/SQLAdvance.sql
@@ -1,120 +1,121 @@
 /* ADVANCE */
 
---/* CTE (COMMON TABLE EXPRESSION) - It's like a function in programming language where you can write code in a set of bracket and give it a name and use it later anywhere.
---  The SELECT statement where the CTE will be called should be right after the CTE table. 
---  It will not work any else or in between of other queries.
---  It is not stored anywhere, it is only created in memory rathen than a tempdb file like a temp table would be but in general a CTE acts very much like a subqurey.
---  It is also called WITH Statement as it starts with WITH.*/
+/* CTE (COMMON TABLE EXPRESSION) - It's like a function in programming language where you can write code in a set of bracket and give it a name and use it later anywhere.
+  The SELECT statement where the CTE will be called should be right after the CTE table. 
+  It will not work any else or in between of other queries.
+  It is not stored anywhere, it is only created in memory rathen than a tempdb file like a temp table would be but in general a CTE acts very much like a subqurey.
+  It is also called WITH Statement as it starts with WITH.*/
 
---WITH CTE_Employee AS 
---( SELECT FirstName, LastName, Gender, Salary,
---COUNT(Gender) OVER (PARTITION BY Gender) T_Gender,
---AVG(Salary) OVER (PARTITION BY Gender) Avg_Sal
---FROM Alex..EmployeeDemographics Demo
---JOIN Alex..EmployeeSalary Sal
---ON Demo.EmployeeID = Sal.EmployeeID
---WHERE Salary > '45000' )
+WITH CTE_Employee AS 
+( SELECT FirstName, LastName, Gender, Salary,
+COUNT(Gender) OVER (PARTITION BY Gender) T_Gender,
+AVG(Salary) OVER (PARTITION BY Gender) Avg_Sal
+FROM Alex..EmployeeDemographics Demo
+JOIN Alex..EmployeeSalary Sal
+ON Demo.EmployeeID = Sal.EmployeeID
+WHERE Salary > '45000' )
 
---SELECT * FROM CTE_Employee
+SELECT * FROM CTE_Employee
 
---/* Following will not work because it can be excuted along with the CTE table due to the SELECT Statement in between */ 
---SELECT FirstName, Avg_Sal FROM CTE_Employee
+/* Following will not work because it can be excuted along with the CTE table due to the SELECT Statement in between */ 
+SELECT FirstName, Avg_Sal FROM CTE_Employee
 
---/* TEMP TABLE - You can call it multiple time unlike CTE. */
+/* TEMP TABLE - You can call it multiple time unlike CTE. */
 
---CREATE TABLE #temp_Employee 
---( EmployeeID int, JobTitle varchar(100), Salary int )
+CREATE TABLE #temp_Employee 
+( EmployeeID int, JobTitle varchar(100), Salary int )
 
---Select * From #temp_employee
+Select * From #temp_employee
 
---Insert into #temp_employee values (
---'1001', 'HR', '45000'
---)
-
-
---/* Selecting all the data from one table and inserting it into other table */
---Insert into #temp_employee 
---SELECT * From Alex..EmployeeSalary
-
---Select * From #temp_employee
+Insert into #temp_employee values (
+'1001', 'HR', '45000'
+)
 
 
+/* Selecting all the data from one table and inserting it into other table */
+Insert into #temp_employee 
+SELECT * From Alex..EmployeeSalary
 
---/* Can add below line when you want to run everything mulitple time.
---   So if the table EXISTS it will get DELETE and then new table will be created, due to which we can see creation and insertion of the table without any error. */
---DROP TABLE IF EXISTS #temp_employee3
-
-
---Create table #temp_employee3 (
---JobTitle varchar(100),
---EmployeesPerJob int ,
---AvgAge int,
---AvgSalary int
---)
-
---/* The following qurey insert the values which are obtained after the excution of the SELECT statement.
---   This helps in saving us from rewriting the SELECT Statement repeatedly, we can just use the TEMP TABLE NAME whenever whereever we want the result of the SELECT Statment or perfrom more operation on the result of the TEMP TABLE, which help in better understanding and reducing the long Quries.*/
---Insert into #temp_employee3
---SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
---FROM Alex..EmployeeDemographics emp
---JOIN Alex..EmployeeSalary sal
---	ON emp.EmployeeID = sal.EmployeeID
---group by JobTitle
-
---Select * 
---From #temp_employee3
-
---SELECT AvgAge * AvgSalary
---from #temp_employee3
-
---/* STRING FUNCTIONS - TRIM, LTRIM, RTRIM, REPLACE, SUBSTRING, UPPER, LOWER */
+Select * From #temp_employee
 
 
---CREATE TABLE EmployeeErrors (
---EmployeeID varchar(50)
---,FirstName varchar(50)
---,LastName varchar(50)
---)
 
---Insert into EmployeeErrors Values 
---('1001  ', 'Jimbo', 'Halbert')
---,('  1002', 'Pamela', 'Beasely')
---,('1005', 'TOby', 'Flenderson - Fired')
+/* Can add below line when you want to run everything mulitple time.
+   So if the table EXISTS it will get DELETE and then new table will be created, due to which we can see creation and insertion of the table without any error. */
+DROP TABLE IF EXISTS #temp_employee3
 
---Select *
---From EmployeeErrors
 
---/* TRIM - It is use to remove extra spaces from any side of the value */
---SELECT EmployeeID, TRIM(EmployeeID) Trim_ID FROM EmployeeErrors
+Create table #temp_employee3 (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
 
---/* RTRIM - It is use to remove extra spaces from Right side of the value */
---SELECT EmployeeID, RTRIM(EmployeeID) RTrim_ID FROM EmployeeErrors
+/* The following qurey insert the values which are obtained after the excution of the SELECT statement.
+   This helps in saving us from rewriting the SELECT Statement repeatedly, we can just use the TEMP TABLE NAME whenever whereever we want the result of the SELECT Statment or perfrom more operation on the result of the TEMP TABLE, which help in better understanding and reducing the long Quries.*/
+Insert into #temp_employee3
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM Alex..EmployeeDemographics emp
+JOIN Alex..EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+group by JobTitle
 
---/* LTRIM - It is use to remove extra spaces from Left side of the value */
---SELECT EmployeeID, LTRIM(EmployeeID) LTrim_ID FROM EmployeeErrors
+Select * 
+From #temp_employee3
 
---/* REPLACE - It's a function with 3 parameters, 
---				1st - the name of the column, 
---				2nd - the value which we want to replace, 
---				3rd - the value with which we want to replace the string. */
+SELECT AvgAge * AvgSalary
+from #temp_employee3
 
---SELECT LastName, REPLACE(LastName,'- Fired', '') LastNameFixed FROM EmployeeErrors
+/* STRING FUNCTIONS - TRIM, LTRIM, RTRIM, REPLACE, SUBSTRING, UPPER, LOWER */
 
---/* SUBSTRING */
 
---Select Substring(err.FirstName,1,3), Substring(dem.FirstName,1,3), Substring(err.LastName,1,3), Substring(dem.LastName,1,3)
---FROM EmployeeErrors err
---JOIN EmployeeDemographics dem
---	on Substring(err.FirstName,1,3) = Substring(dem.FirstName,1,3)
---	and Substring(err.LastName,1,3) = Substring(dem.LastName,1,3)
+CREATE TABLE EmployeeErrors (
+EmployeeID varchar(50)
+,FirstName varchar(50)
+,LastName varchar(50)
+)
 
---/* UPPER & lower */
+Insert into EmployeeErrors Values 
+('1001  ', 'Jimbo', 'Halbert')
+,('  1002', 'Pamela', 'Beasely')
+,('1005', 'TOby', 'Flenderson - Fired')
 
---Select firstname, LOWER(firstname)
---from EmployeeErrors
+Select *
+From EmployeeErrors
 
---Select Firstname, UPPER(FirstName)
---from EmployeeErrors
+/* TRIM - It is use to remove extra spaces from any side of the value */
+SELECT EmployeeID, TRIM(EmployeeID) Trim_ID FROM EmployeeErrors
+
+/* RTRIM - It is use to remove extra spaces from Right side of the value */
+SELECT EmployeeID, RTRIM(EmployeeID) RTrim_ID FROM EmployeeErrors
+
+/* LTRIM - It is use to remove extra spaces from Left side of the value */
+SELECT EmployeeID, LTRIM(EmployeeID) LTrim_ID FROM EmployeeErrors
+
+/* REPLACE - It's a function with 3 parameters, 
+				1st - the name of the column, 
+				2nd - the value which we want to replace, 
+				3rd - the value with which we want to replace the string. */
+
+SELECT LastName, REPLACE(LastName,'- Fired', '') LastNameFixed FROM EmployeeErrors
+
+/* SUBSTRING */
+
+Select Substring(err.FirstName,1,3), Substring(dem.FirstName,1,3), Substring(err.LastName,1,3), Substring(dem.LastName,1,3)
+FROM EmployeeErrors err
+JOIN EmployeeDemographics dem
+	on Substring(err.FirstName,1,3) = Substring(dem.FirstName,1,3)
+	and Substring(err.LastName,1,3) = Substring(dem.LastName,1,3)
+
+/* UPPER & lower */
+
+Select firstname, LOWER(firstname)
+from EmployeeErrors
+
+Select Firstname, UPPER(FirstName)
+from EmployeeErrors
+
 
 /* STORED PROCEDURES - These are like function in programming, which we can execute again and again */
 
